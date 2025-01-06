ef097576b6ad139da7517aade65d7cb1d37da1d0
Forrest Townsend <forrest.town18@gmail.com>
2025-01-06 05:57:49 +0000

    fix test expected-output file name, formatting

--- test/test_cli.py
+++ test/test_cli.py
@@ -6,17 +6,17 @@ def test_cli():
     return_code = subprocess.call(["python3", "directories.py", "-f", "test/input.txt"])
 
     assert return_code == 0
-    
+
     # Find the most recent log file in logs directory
     log_files = os.listdir("logs")
     sorted_log_files = sorted(log_files, reverse=True)
     latest_log_file = sorted_log_files[0]
-    
+
     print(f"Latest log file: {latest_log_file}")
 
     with (
         open(f"logs/{latest_log_file}", "r") as result_file,
-        open("test/output.txt", "r") as expected_output,
+        open("test/expected-output.txt", "r") as expected_output,
     ):
         assert len(result_file.readlines()) == len(expected_output.readlines())
 
